6190750ed1ac28f1213aebf05e13bb4073431803
himalaya-dt <hyperzy.yang@gmail.com>
2020-08-14 10:45:07 -0400

    migrate to new pc

--- include/inpainting2d.h
+++ include/inpainting2d.h
@@ -16,11 +16,18 @@ struct MyPoint2d {
 
 class Inpainting2d {
     void __preProcess(std::vector<dtype> &pre, std::vector<dtype> &cur, const cv::Mat &input_img, const cv::Mat &mask);
+    void __preProcess(std::vector<dtype> &d_pre, std::vector<dtype> &d_cur, std::vector<dtype> &pre, std::vector<dtype> &cur, const cv::Mat &input_img, const cv::Mat &mask);
     void __genResult(const std::vector<dtype> &arr, cv::Mat &output_img);
+    void __genResult(const std::vector<dtype> &arr, cv::Mat &output_img, const cv::Mat &input_img);
+    void __genMask(const cv::Mat &input_img);
+    void __findComputeArea();
+    template <class T>
+    T __getVal(const std::vector<T> &arr, int i, int j);
     static dtype __getSum(const cv::Mat &input_img, const cv::Mat &mask);
     std::vector<MyPoint2d> __computed_area;
-    dtype __n_iterations;
+    int __n_iterations;
     dtype __time_step;
+    cv::Mat __mask;
 public:
     Inpainting2d(int height, int width);
     int h, w;
@@ -30,6 +37,8 @@ public:
     void setTimeStep(dtype ts);
     void heatDiffusion(const cv::Mat &input_img, cv::Mat &output_img, const cv::Mat &mask);
     void anisotropicDiffusion(const cv::Mat &input_img, cv::Mat &output_img, const cv::Mat &mask);
+    void acceleratedTVDiffusion(const cv::Mat &input_img, cv::Mat &output_img, const cv::Mat &mask);
+    void acceleratedBeltramiDiffusion(const cv::Mat &input_img, cv::Mat &output_img, const cv::Mat &mask);
 };
 
 inline unsigned long Inpainting2d::index(int i, int j) {
@@ -38,5 +47,10 @@ inline unsigned long Inpainting2d::index(int i, int j) {
 
 inline bool Inpainting2d::isValidRange(int i, int j) {
     return i >= 0 && i < h && j >= 0 && j < w;
+}
+
+template <class T>
+inline T Inpainting2d::__getVal(const std::vector<T> &arr, int i, int j) {
+    return this->isValidRange(i, j) ? arr[this->index(i, j)] : 0;
 }
 #endif //DIFFU_INPAINTING_INPAINTING2D_H
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include <memory>
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include <opencv2/rgbd.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 
@@ -17,7 +20,7 @@ int main(int argc, char *argv[]) {
         cout << "Usage: " << endl;
         cout << "         heat: ./diffu_inpainting heat [number of iteration (int)] [time step (float)] [input img path] [mask path] [output img path]" << endl;
         cout << "  anisotropic: ./diffu_inpainting anisotropic [number of iteration (int)] [time step (float)] [input img path] [mask path] [output img path]" << endl;
-        cout << "     draw mask: ./diffu_inpainting draw [input img path] [output mask path]" << endl;
+        cout << "    draw mask: ./diffu_inpainting draw [input img path] [output mask path]" << endl;
         exit(EXIT_FAILURE);
     }
 //    genMask(depth_path);
@@ -31,26 +34,52 @@ int main(int argc, char *argv[]) {
         d_img->loadKinect(depth_path);
         d_img->showImage();
 
-        string mask_path(argv[5]);
-        cv::Mat mask = cv::imread(mask_path, 0);
-        auto inpaint(make_unique<Inpainting2d>(mask.rows, mask.cols));
-
-        cv::Mat ans;
-        inpaint->setIterationTimes(stoi(string(argv[2])));
-        inpaint->setTimeStep(stof(string(argv[3])));
-        if (string(argv[1]) == "heat") {
-            inpaint->heatDiffusion(d_img->getImage(), ans, mask);
-        } else if (string(argv[1]) == "anisotropic") {
-            inpaint->anisotropicDiffusion(d_img->getImage(), ans, mask);
-        } else {
-            cout << "Unknown method" << endl;
-            exit(EXIT_FAILURE);
-        }
+        /*** try opencv depth cleaner ****/
+        cv::rgbd::DepthCleaner *depthc = new cv::rgbd::DepthCleaner(CV_16U, 5, cv::rgbd::DepthCleaner::DEPTH_CLEANER_NIL);
+        cv::Mat out(d_img->getHeight(), d_img->getWidth(), CV_16U);
+        depthc->operator()(d_img->getImage(), out);
+//        cv::Mat out = d_img->getImage();
+//        out = d_img->getImage();
+//        string mask_path(argv[5]);
+//        cv::Mat mask = cv::imread(mask_path, 0);
+//        cv::threshold(mask, mask, 125, 255, CV_THRESH_BINARY);
+        cv::inpaint(out, (out == 0), out, 5, cv::INPAINT_TELEA);
         string output_path(argv[6]);
-        cv::imwrite(output_path, ans);
+        cv::imwrite(output_path, out);
         auto res_img(make_unique<DepthImage>());
         res_img->loadKinect(output_path);
         res_img->showImage();
+
+
+
+//         string mask_path(argv[5]);
+//         cv::Mat mask = cv::imread(mask_path, 0);
+//         auto inpaint(make_unique<Inpainting2d>(d_img->getHeight(), d_img->getWidth()));
+//
+//         cv::Mat ans;
+//         inpaint->setIterationTimes(stoi(string(argv[2])));
+//         inpaint->setTimeStep(stof(string(argv[3])));
+//         if (string(argv[1]) == "heat") {
+//             inpaint->heatDiffusion(d_img->getImage(), ans, mask);
+//         } else if (string(argv[1]) == "anisotropic") {
+//             inpaint->anisotropicDiffusion(d_img->getImage(), ans, mask);
+//         } else if (string(argv[1]) == "acceleratedTV") {
+//             inpaint->acceleratedTVDiffusion(d_img->getImage(), ans, mask);
+//         } else if (string(argv[1]) == "acceleratedBel") {
+//             // pre_process missing area first
+//             cv::Mat in;
+////             inpaint->heatDiffusion(d_img->getImage(), in, mask);
+//
+//             inpaint->acceleratedBeltramiDiffusion(d_img->getImage(), ans, mask);
+//         } else {
+//                 cout << "Unknown method" << endl;
+//                 exit(EXIT_FAILURE);
+//         }
+//         string output_path(argv[6]);
+//         cv::imwrite(output_path, ans);
+//         auto res_img(make_unique<DepthImage>());
+//         res_img->loadKinect(output_path);
+//         res_img->showImage();
         return 0;
     }
 }
